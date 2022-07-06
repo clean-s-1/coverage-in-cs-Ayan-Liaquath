917e4c0c4facb90ba0e8885701f96747be285910
Ayan-Liaquath <105066915+Ayan-Liaquath@users.noreply.github.com>
2022-07-07 00:46:27 +0530

    Create AlertStatus.cs

---
+++ TypewiseAlert/AlertStatus.cs
@@ -0,0 +1,15 @@
+namespace TypewiseAlert
+{
+    public enum AlertStatus
+    {
+        Success,
+
+        Alert_Target_Is_Not_Present,
+
+        Alerter_Is_Not_Valid,
+
+        Printer_Action_Is_Invalid,
+
+        Failed
+    }
+}
